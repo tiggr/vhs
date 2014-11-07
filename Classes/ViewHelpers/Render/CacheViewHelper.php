c4d0dc8b69cf6cd80948fcc905c8125d0066677b
Daniel Schöne <daniel@schoene.it>
2014-11-07 15:47:21 +0100

    Fixes cache identifier issue w/ namespaces

--- Classes/ViewHelpers/Render/CacheViewHelper.php
+++ Classes/ViewHelpers/Render/CacheViewHelper.php
@@ -65,6 +65,8 @@ class CacheViewHelper extends AbstractRenderViewHelper {
 
 	const ID_SEPARATOR = '-';
 
+	const CACHE_ID = 'v-render-cache';
+
 	/**
 	 * @var \TYPO3\CMS\Core\Cache\Frontend\StringFrontend
 	 */
@@ -113,7 +115,7 @@ class CacheViewHelper extends AbstractRenderViewHelper {
 	 * @return boolean
 	 */
 	protected function has($id) {
-		return (boolean) $this->cache->has(get_class($this) . self::ID_SEPARATOR . $id);
+		return (boolean) $this->cache->has(self::CACHE_ID . self::ID_SEPARATOR . $id);
 	}
 
 	/**
@@ -122,7 +124,7 @@ class CacheViewHelper extends AbstractRenderViewHelper {
 	 * @return void
 	 */
 	protected function store($value, $id) {
-		$this->cache->set(get_class($this) . self::ID_SEPARATOR . $id, $value);
+		$this->cache->set(self::CACHE_ID . self::ID_SEPARATOR . $id, $value);
 	}
 
 	/**
@@ -130,8 +132,8 @@ class CacheViewHelper extends AbstractRenderViewHelper {
 	 * @return mixed
 	 */
 	protected function retrieve($id) {
-		if ($this->cache->has(get_class($this) . self::ID_SEPARATOR . $id)) {
-			return $this->cache->get(get_class($this) . self::ID_SEPARATOR . $id);
+		if ($this->cache->has(self::CACHE_ID . self::ID_SEPARATOR . $id)) {
+			return $this->cache->get(self::CACHE_ID . self::ID_SEPARATOR . $id);
 		}
 		return NULL;
 	}
